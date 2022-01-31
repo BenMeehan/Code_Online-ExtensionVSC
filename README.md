580b685e3c4582fd8937e7df3b0b0d84ca561667
ben meehan <benmeehan111@gmail.com>
2022-01-31 14:08:22 +0530

    Added Synchronous File Management

---
+++ README.md
@@ -0,0 +1,31 @@
+# Welcome to Code Online Extension
+
+Code Online is a free extension for  Visual Studio Code which allows you to run your program completely online directly from your editor without the need for a compiler to be installed in the local machine.
+
+It is perfect if you want to try out a try language or quickly execute a program in VS Code without installing a compiler.
+
+**NOTE:**
+This extension does not support GUI apps.
+
+## Supported Languages
+C++, C, C#, Java, Python, Ruby, Kotlin, Swift
+
+... more to be added soon.
+
+
+# How to use
+
+Simply press ***Ctrl+Shift+p*** in your keyboard to open the command palette. 
+Now type ***run*** and press *enter*.
+
+![demo.gif](https://s10.gifyu.com/images/Code-Online1.gif)
+
+Thats it!
+
+You can view the output of your program in the ***output.txt*** file in the same directory.
+
+## Providing custom input
+
+If you wan't to provide custom input to your program, create a file named ***input.txt*** in the same directory and enter the input in that file.
+
+![inputDemo.gif](https://s10.gifyu.com/images/Code-Online2.gif)
---
+++ extension.js
@@ -0,0 +1,95 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const fs=require("fs");
+var axios = require('axios');
+
+// Supported Languages
+var langs=["cpp","c","py","java","rb","cs","kt","swift"]
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "codeonline" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('codeonline.run', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Running code in CodeX...');
+
+		// Get the required file paths
+		var currentlyOpenFile = vscode.window.activeTextEditor?.document.uri.fsPath;
+		var inputFile = currentlyOpenFile.substring(0, currentlyOpenFile.lastIndexOf('\\'))+"\\input.txt";
+		var outputFile=currentlyOpenFile.substring(0, currentlyOpenFile.lastIndexOf('\\'))+"\\output.txt";
+
+		// Variable to store the code, language and custom inputs
+		var content={
+			"code":"",
+			"language": "",
+			"input":""
+		}
+		
+		// Get language from file extension
+		content["language"]=currentlyOpenFile.substring(currentlyOpenFile.lastIndexOf('.')+1);
+
+		// Check if valid language
+		if(!langs.includes(content["language"])){
+			vscode.window.showInformationMessage('Error: Unsupported File/Language');
+		}else{
+			// Read code from current active file
+			content["code"]=fs.readFileSync(currentlyOpenFile,{encoding:"utf-8"});
+
+			// Read input from input.txt file
+			if(fs.existsSync(inputFile)){
+				content["input"]=fs.readFileSync(inputFile,{encoding:"utf-8"});
+			}
+
+			// Convert the data to JSON
+			var data=JSON.stringify(content);
+
+			// Configuration for CodeX API
+			var config = {
+				method: 'post',
+				url: 'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+				headers: { 
+				'Content-Type': 'application/json'
+				},
+				data : data
+			};
+			
+			// Making an axios request to the API with the JSON content
+			axios(config)
+			.then(function (response) {
+				fs.writeFileSync(outputFile,response.data.output);
+			})
+			.catch(function (error) {
+				fs.writeFileSync(outputFile,error);
+			});
+
+			// Message to user
+			vscode.window.showInformationMessage('Check the output.txt file...');
+
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
